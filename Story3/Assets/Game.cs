5be7394ab18feae1331c0d9967599c7bbd9038cb
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-01-03 18:20:12 -0500

    Sunday new commit

--- Story3/Assets/Game.cs
+++ Story3/Assets/Game.cs
@@ -8,16 +8,28 @@ public class Game : MonoBehaviour
 {
 
   [SerializeField] Text textcompontent;
+  [SerializeField] Text textcompontenttwo;
 
     // Start is called before the first frame update
     void Start()
     {
-        textcompontent.text = ("welcome to the Game");
+        if (textcompontent == false)
+        {
+            textcompontent.text = ("Game will process");
+
+        }
+
+        else
+        {
+            textcompontenttwo.text = ("Game start");
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
 
 
     }
