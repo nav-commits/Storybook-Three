cf732443cc6a400a2429b722ef3adab59493bb65
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-01-12 17:25:07 -0500

    added slider to play, stop and pause the video

---
+++ Story3/Assets/trackscript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.EventSystems;
+
+public class trackscript : MonoBehaviour
+{
+    public VideoPlayer video;
+    Slider tracker;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tracker = GetComponent<Slider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tracker.value = (float)video.frame / (float)video.frameCount;
+    }
+}
+
+
