59e436098d035512b6298a20456c3df9e502e5bf
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-01-12 18:21:38 -0500

    added more functionality to the play,pause and stop

--- Story3/Assets/trackscript.cs
+++ Story3/Assets/trackscript.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 using UnityEngine.Video;
 using UnityEngine.EventSystems;
 
-public class trackscript : MonoBehaviour
-{
+public class trackscript : MonoBehaviour,IPointerDownHandler,IPointerUpHandler{
+
     public VideoPlayer video;
     Slider tracker;
-
+    bool slide = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,10 +18,31 @@ public class trackscript : MonoBehaviour
 
     }
 
+
+    public void OnPointerDown(PointerEventData a)
+    {
+        slide = true;
+    }
+
+
+
+    public void OnPointerUp(PointerEventData a)
+    {
+        float frame = (float)tracker.value * (float)video.frameCount;
+        video.frame = (long)frame;
+        slide = false;
+    }
+
+
+
+
     // Update is called once per frame
     void Update()
     {
-        tracker.value = (float)video.frame / (float)video.frameCount;
+        if (!slide)
+        {
+            tracker.value = (float)video.frame / (float)video.frameCount;
+        }
     }
 }
 
